5e2113a5820f8d0babead74995af12dea64ccc02
Antonin <antoriche.ar@gmail.com>
2017-12-05 18:39:40 +0100

    bug git update

---
+++ Assets/Scripts/GUI/ActionsGUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ActionsGUI : MonoBehaviour {
+
+	public ActionMapper actions;
+	public RectTransform actionGUIPrefab;
+
+	private List<ActionGUI> list = new List<ActionGUI>();
+	private Player player;
+
+	void Start(){
+		player = GameObject.FindWithTag ("Player").GetComponent<Player> ();
+		int i = 1;
+		foreach (ActionLine actionline in actions.map) {
+			Action action = actionline.action;
+			RectTransform o = Instantiate (actionGUIPrefab,new Vector2(32,32*(i*2)-32),Quaternion.identity);
+			o.transform.parent = this.transform;
+			o.GetComponentInChildren<Image>().sprite = action.Image;
+			o.Find("Key Panel").GetComponentInChildren<Text>().text = actionline.character.ToString();
+			list.Add ( new ActionGUI{GUI = o , Action = action} );
+			i++;
+		}
+	}
+
+	void OnGUI(){
+		foreach (ActionGUI actionGUI in list) {
+			int loadingTime = ActionManager.Instance.GetLoadingTime (player,actionGUI.Action);
+			if (loadingTime > 0) {
+				actionGUI.GUI.Find ("Loading").GetComponentInChildren<Text> ().text = loadingTime.ToString ();
+				actionGUI.GUI.GetComponentInChildren<Image> ().color = Color.grey;
+			} else {
+				actionGUI.GUI.Find ("Loading").GetComponentInChildren<Text> ().text = "";
+				actionGUI.GUI.GetComponentInChildren<Image> ().color = Color.white;
+			}
+		}
+	}
+
+
+}
+
+class ActionGUI{
+	
+	public Action Action;
+	public RectTransform GUI;
+}
---
+++ Assets/Scripts/GUI/LifeBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum AnchorPoint {LEFT = 0, RIGHT = 1}
+
+public class LifeBar : MonoBehaviour {
+
+	RectTransform progress;
+
+	public AnchorPoint anchor;
+	public Entity entity;
+
+	Vector2 size;
+
+	// Use this for initialization
+	void Start () {
+		size = this.GetComponent<RectTransform> ().localScale;
+		Transform t = transform.FindChild ("ProgressBar");
+		if (!t) {
+			Debug.LogError ("LifeBar must have a child named ProgressBar");
+			return;
+		}
+		progress = t.GetComponent<RectTransform> ();
+	}
+
+	void OnGUI () {
+		this.GetComponent<RectTransform> ().localScale = entity ? size : Vector2.zero;
+		if (!entity)
+			return;
+		switch (anchor) {
+		case AnchorPoint.LEFT:
+			progress.anchorMax = new Vector2(((float)entity.Life) / entity.MaxLife,1f);
+			break;
+		case AnchorPoint.RIGHT:
+			progress.anchorMin = new Vector2(1-((float)entity.Life / entity.MaxLife),0f);
+			break;
+		}
+	}
+}
